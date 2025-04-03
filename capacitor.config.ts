2654e2dc7d8267e9890a36fcf577ee24f7873939
Maia-Aedo <aedomaia10@gmail.com>
2025-04-03 15:47:27 -0300

    Config inicial y estructura de carpetas

---
+++ capacitor.config.ts
@@ -0,0 +1,8 @@
+import { CapacitorConfig } from '@capacitor/cli';
+const config: CapacitorConfig = {
+  appId: "io.ionic.starter",
+  appName: "Ionic--asistencia",
+  webDir: "www",
+};
+
+export default config;
